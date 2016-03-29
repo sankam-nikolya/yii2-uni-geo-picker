3dfc02a6c71e82acaf106eed661db24be43d7ab4
Jakeroid <jakeroid.s@gmail.com>
2016-03-29 18:33:27 +0300

    Added uni geo picker view.

--- src/views/uni-geo-picker.php
+++ src/views/uni-geo-picker.php
@@ -6,12 +6,15 @@
  * Time: 16:41
  */
 
-use jakeroid\liqugallerywidget\UniGeoPickerAsset;
+use jakeroid\unigeopicker\UniGeoPickerAsset;
+use yii\helpers\Html;
 
 /* @var $this yii\web\View */
 /* @var $form \yii\widgets\ActiveForm */
 /* @var $model \yii\base\Model */
 /* @var $attribute string */
+/* @var $default_center_coordinates [] */
+/* @var $zoom_level string */
 
 UniGeoPickerAsset::register($this);
 ?>
@@ -19,13 +22,18 @@ UniGeoPickerAsset::register($this);
 <div
     id="uni-geo-picker-widget"
     class="panel panel-default"
+    data-zoom-level="<?= $zoom_level ?>"
+    data-default-center-coordinates="<?= $default_center_coordinates ?>"
 >
     <div class="panel-heading">
         <button type="button" class="btn btn-default no-focus spoiler-trigger" data-toggle="collapse"><?= Yii::t('app', 'Geo Picker') ?></button>
     </div>
     <div class="panel-collapse collapse out">
         <div class="panel-body">
+            <div id="uni-geo-picker-map">
+                
+            </div>
         </div>
     </div>
-    <?= $form->field($model, $attribute)->hiddenInput(['id' => 'uni-geo-picker-input']) ?>
+    <?= Html::activeHiddenInput($model, $attribute, ['id' => 'uni-geo-picker-hidden-input']) ?>
 </div>
